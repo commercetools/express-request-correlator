16ac28dbfc55f712b8825eff7d56257f9d7ccb21
tdeekens <tobias.deekens@commercetools.de>
2019-11-29 13:28:04 +0100

    feat: type check on commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  '**/*.js': ['npm run fix:eslint', 'npm run format:js', 'git add'],
+  '**/*.js': [
+    'npm run fix:eslint',
+    'npm run format:js',
+    'git add',
+    'flow focus-check',
+  ],
   '*.md': ['npm run format:md', 'git add'],
 };
